a09966f2e3e8d8ec586647c995c27ddae15c36a8
rubnsn <whiteruby.0214@gmail.com>
2013-09-27 13:52:28 +0900

    Fix top drawing

--- ruby/bamboo/render/magatama/RenderGravityHole.java
+++ ruby/bamboo/render/magatama/RenderGravityHole.java
@@ -21,10 +21,10 @@ public class RenderGravityHole extends Render{
 	private static final ResourceLocation TEX_END_SKY = new ResourceLocation("textures/environment/end_sky.png");
     private static final ResourceLocation TEX_END_PORTAL = new ResourceLocation("textures/entity/end_portal.png");
 	private static final Random RAND = new Random(31100L);
-	private Sphere s = new Sphere();
+	private Sphere sphere = new Sphere();
     FloatBuffer floatBuffer = GLAllocation.createDirectFloatBuffer(16);
     public RenderGravityHole(){
-    	s.setTextureFlag(true);
+    	sphere.setTextureFlag(true);
     }
 	@Override
 	public void doRender(Entity entity, double d0, double d1, double d2,
@@ -33,28 +33,30 @@ public class RenderGravityHole extends Render{
 	}
 	private void render (EntityGravityHole entity, double posX, double posY, double posZ){
 	
-	    float f1 = (float)this.renderManager.viewerPosX;
-	    float f2 = (float)this.renderManager.viewerPosY;
-	    float f3 = (float)this.renderManager.viewerPosZ;
+	    float viewerPosX = (float)this.renderManager.viewerPosX;
+	    float viewerPosY = (float)this.renderManager.viewerPosY;
+	    float viewerPosZ = (float)this.renderManager.viewerPosZ;
 	    GL11.glDisable(GL11.GL_LIGHTING);
 	    RAND.setSeed(31100L);
-	    float f4 = 0.75F;
+	    float f4 = 26F;
 	
 	    for (int i = 0; i < 16; ++i)
 	    {
 	        GL11.glPushMatrix();
 	        float f5 = (float)(16 - i);
-	        float f6 = 0.0625F;
-	        float f7 = 1.0F / (f5 + 1.0F);
+	        float scale = 0.0625F;
+	        float color = 1.0F / (f5 + 1.0F);
 	
 	        if (i == 0)
 	        {
 	            this.func_110776_a(TEX_END_SKY);
-	            f7 = 0.1F;
+	            color = 0.1F;
 	            f5 = 65.0F;
-	            f6 = 0.125F;
+	            scale = 0.125F;
 	            GL11.glEnable(GL11.GL_BLEND);
 	            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+	    		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
 	        }
 	
 	        if (i == 1)
@@ -62,15 +64,15 @@ public class RenderGravityHole extends Render{
 	            this.func_110776_a(TEX_END_PORTAL);
 	            GL11.glEnable(GL11.GL_BLEND);
 	            GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
-	            f6 = 0.5F;
+	            scale = 0.5F;
 	        }
-	
+
 	        float f8 = (float)(-(posY + (double)f4));
 	        float f9 = f8 + ActiveRenderInfo.objectY;
 	        float f10 = f8 + f5 + ActiveRenderInfo.objectY;
 	        float f11 = f9 / f10;
 	        f11 += (float)(posY + (double)f4);
-	        GL11.glTranslatef(f1, f11, f3);
+	        GL11.glTranslatef(viewerPosX, f11, viewerPosZ);
 	        GL11.glTexGeni(GL11.GL_S, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR);
 	        GL11.glTexGeni(GL11.GL_T, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR);
 	        GL11.glTexGeni(GL11.GL_R, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR);
@@ -88,13 +90,14 @@ public class RenderGravityHole extends Render{
 	        GL11.glPushMatrix();
 	        GL11.glLoadIdentity();
 	        GL11.glTranslatef(0.0F, (float)(Minecraft.getSystemTime() % 700000L) / 700000.0F, 0.0F);
-	        GL11.glScalef(f6, f6, f6);
+	        scale=entity.getScale();
+	        GL11.glScalef(scale, scale, scale);
 	        GL11.glTranslatef(0.5F, 0.5F, 0.0F);
 	        GL11.glRotatef((float)(i * i * 4321 + i * 9) * 2.0F, 0.0F, 0.0F, 1.0F);
 	        GL11.glTranslatef(-0.5F, -0.5F, 0.0F);
-	        GL11.glTranslatef(-f1, -f3, -f2);
+	        GL11.glTranslatef(-viewerPosX, -viewerPosZ, -viewerPosY);
 	        f9 = f8 + ActiveRenderInfo.objectY;
-	        GL11.glTranslatef(ActiveRenderInfo.objectX * f5 / f9, ActiveRenderInfo.objectZ * f5 / f9, -f2);
+	        GL11.glTranslatef(ActiveRenderInfo.objectX * f5 / f9, ActiveRenderInfo.objectZ * f5 / f9, -viewerPosY);
 
 	        GL11.glPopMatrix();
 	        GL11.glMatrixMode(GL11.GL_MODELVIEW);
@@ -108,10 +111,10 @@ public class RenderGravityHole extends Render{
 	            f11 = 1.0F;
 	        }
 	        GL11.glPushMatrix();
-	        GL11.glColor4f(f11 * f7, f12 * f7, f13 * f7, 1.0F);
+	        GL11.glColor4f(f11 * color, f12 * color, f13 * color, 1.0F);
 	        GL11.glScalef(1.0F,1.0F,1.0F);
 	        GL11.glTranslatef((float)posX, (float)posY+(entity.height/2), (float)posZ);
-			s.draw(entity.getScale(), 16, 16);
+			sphere.draw(entity.getScale(), 16, 16);
 			GL11.glPopMatrix();
 	    }
 	
