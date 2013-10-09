8911da27f601aa8cdac846e8e7ac6e2a89aeccc4
rubnsn <whiteruby.0214@gmail.com>
2013-10-09 21:27:33 +0900

    Fix extra drop of Katana

---
+++ ruby/bamboo/item/KatanaDropItem.java
@@ -0,0 +1,27 @@
+package ruby.bamboo.item;
+
+import net.minecraft.item.ItemStack;
+
+public class KatanaDropItem {
+	private ItemStack dropItem;
+	private float reality;
+	private int randomAmount;
+	public KatanaDropItem(ItemStack dropItem,float reality){
+		this.dropItem=dropItem;
+		this.reality=reality;
+		this.randomAmount=0;
+	}
+	public KatanaDropItem setRandomAddAmount(int amount){
+		this.randomAmount=amount;
+		return this;
+	}
+	public float getReality(){
+		return reality;
+	}
+	public ItemStack getDropItem() {
+		return dropItem;
+	}
+	public int getRandomAmount() {
+		return randomAmount;
+	}
+}
