6712b129f8b3d5f9dcf58c4817dbbd4608724d5c
rubnsn <whiteruby.0214@gmail.com>
2014-02-02 16:50:44 +0900

    1個は種のドロップを保証する

--- ruby/bamboo/block/BlockRicePlant.java
+++ ruby/bamboo/block/BlockRicePlant.java
@@ -1,10 +1,14 @@
 package ruby.bamboo.block;
 
+import java.util.ArrayList;
+
 import ruby.bamboo.BambooInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
+import net.minecraft.world.World;
 
 public class BlockRicePlant extends BlockCrops {
     public BlockRicePlant(int par1) {
@@ -24,5 +28,14 @@ public class BlockRicePlant extends BlockCrops {
     @Override
     protected int getCropItem() {
         return BambooInit.strawIID;
+    }
+
+    @Override
+    public ArrayList<ItemStack> getBlockDropped(World world, int x, int y, int z, int metadata, int fortune) {
+        ArrayList<ItemStack> ret = super.getBlockDropped(world, x, y, z, metadata, fortune);
+        if (metadata >= 7) {
+            ret.add(new ItemStack(this.getSeedItem(), 1, 0));
+        }
+        return ret;
     }
 }
