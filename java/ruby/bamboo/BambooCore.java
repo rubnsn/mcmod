d3a9c3caf65dec064d81fb697a8fa6cfe40b407f
rubnsn <whiteruby.0214@gmail.com>
2014-04-20 13:05:16 +0900

    FML側で処理されそうなのでMissingの処理削除

--- java/ruby/bamboo/BambooCore.java
+++ java/ruby/bamboo/BambooCore.java
@@ -1,11 +1,6 @@
 package ruby.bamboo;
 
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import net.minecraft.block.BlockDispenser;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.oredict.RecipeSorter;
 import ruby.bamboo.dispenser.DispenserBehaviorBambooSpear;
@@ -14,22 +9,17 @@ import ruby.bamboo.dispenser.DispenserBehaviorFireCracker;
 import ruby.bamboo.gui.GuiHandler;
 import ruby.bamboo.proxy.CommonProxy;
 import ruby.bamboo.world.WorldProviderBamboo;
-
-import com.google.common.collect.Maps;
-
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
-import cpw.mods.fml.common.event.FMLModIdMappingEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.event.FMLServerStoppedEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.relauncher.CoreModManager;
 import cpw.mods.fml.relauncher.ReflectionHelper;
 import cpw.mods.fml.relauncher.ReflectionHelper.UnableToAccessFieldException;
@@ -124,52 +114,5 @@ public class BambooCore {
             result = false;
         }
         return result;
-    }
-
-    @Mod.EventHandler
-    public void mappingChanged(FMLModIdMappingEvent evt) {
-        try {
-            this.invokePrivateMethod(GameData.itemRegistry, "beginIdSwap");
-            Map<String, Integer> missingItem = Maps.newHashMap((Map) this.invokePrivateMethod(GameData.itemRegistry, "getMissingMappings"));
-            Class[] swapClazz = { int.class, String.class, Object.class };
-            for (Entry<String, Integer> entry : missingItem.entrySet()) {
-                int blockId = GameData.blockRegistry.getId(entry.getKey());
-                int itemId = GameData.itemRegistry.getId(entry.getKey());
-                if (GameData.itemRegistry.get(entry.getKey()) instanceof ItemBlock) {
-                    this.invokePrivateMethod(GameData.itemRegistry, "swap", swapClazz, blockId, entry.getKey(), GameData.itemRegistry.get(entry.getKey()));
-                } else {
-                    this.invokePrivateMethod(GameData.itemRegistry, "swap", swapClazz, itemId, entry.getKey(), GameData.itemRegistry.get(entry.getKey()));
-                }
-            }
-            this.invokePrivateMethod(GameData.itemRegistry, "completeIdSwap");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Object invokePrivateMethod(Object ins, String name, Object... param) {
-        Class[] clazz = new Class[param.length];
-        for (int i = 0; i < clazz.length; i++) {
-            clazz[i] = param[i].getClass();
-        }
-        return invokePrivateMethod(ins, name, clazz, param);
-    }
-
-    private Object invokePrivateMethod(Object ins, String name, Class[] clazz, Object... param) {
-        try {
-            Class cls = ins.getClass();
-            if (param.length > 0) {
-                Method method = cls.getDeclaredMethod(name, clazz);
-                method.setAccessible(true);
-                return method.invoke(ins, param);
-            } else {
-                Method method = cls.getDeclaredMethod(name);
-                method.setAccessible(true);
-                return method.invoke(ins);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
