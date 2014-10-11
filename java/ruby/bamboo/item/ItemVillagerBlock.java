49b21a3d0777f150e4b30c17f59846c44d2add02
rubnsn <whiteruby.0214@gmail.com>
2014-10-11 22:52:53 +0900

    村人風ブロックを頭にかぶっている際、ダメージ音を変更する

--- java/ruby/bamboo/item/ItemVillagerBlock.java
+++ java/ruby/bamboo/item/ItemVillagerBlock.java
@@ -2,14 +2,19 @@ package ruby.bamboo.item;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemVillagerBlock extends ItemBlock {
 
     public ItemVillagerBlock(Block p_i45328_1_) {
         super(p_i45328_1_);
         this.setMaxStackSize(1);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
@@ -18,5 +23,21 @@ public class ItemVillagerBlock extends ItemBlock {
             return true;
         }
         return false;
+    }
+
+    @SubscribeEvent
+    public void onSoundPlayEvent(PlaySoundAtEntityEvent event) {
+        if (event.entity instanceof EntityPlayer) {
+            ItemStack is = ((EntityPlayer) event.entity).getEquipmentInSlot(4);
+            if (is != null && is.getItem() == this) {
+                if (event.name.equals("game.player.hurt")) {
+                    if (((EntityPlayer) event.entity).getHealth() % 3 == 0) {
+                        event.name = "mob.villager.idle";
+                    } else {
+                        event.name = "mob.villager.hit";
+                    }
+                }
+            }
+        }
     }
 }
