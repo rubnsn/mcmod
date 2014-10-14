2125bd973b677c4eba948f2549412992c9600262
rubnsn <whiteruby.0214@gmail.com>
2014-10-15 07:38:45 +0900

    海藻→海草

--- java/ruby/bamboo/worldgen/WorldGenSakura.java
+++ java/ruby/bamboo/worldgen/WorldGenSakura.java
@@ -1,7 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode
-
 package ruby.bamboo.worldgen;
 
 import java.util.Random;
@@ -11,10 +7,6 @@ import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import ruby.bamboo.BambooInit;
-
-// Referenced classes of package net.minecraft.src:
-//            WorldGenerator, World, Block, BlockLeaves,
-//            BlockGrass
 
 public class WorldGenSakura extends WorldGenAbstractTree {
     int type;
@@ -29,83 +21,72 @@ public class WorldGenSakura extends WorldGenAbstractTree {
     }
 
     @Override
-    public boolean generate(World world, Random random, int i, int j, int k) {
+    public boolean generate(World world, Random random, int posX, int posY, int posZ) {
         int l;
         boolean flag;
-        label0: {
-            l = random.nextInt(3) + 4;
-            flag = true;
 
-            if (j >= 1) {
-                world.getClass();
+        l = random.nextInt(3) + 4;
+        flag = true;
 
-                if (j + l + 1 <= 128) {
-                    break label0;
-                }
-            }
-
+        if (posY < 1 || posY + l + 1 > 128) {
             return false;
         }
-        label1: {
-            for (int i1 = j; i1 <= j + 1 + l; i1++) {
-                byte byte0 = 1;
 
-                if (i1 == j) {
-                    byte0 = 0;
-                }
+        for (int i1 = posY; i1 <= posY + 1 + l; i1++) {
+            byte byte0 = 1;
 
-                if (i1 >= (j + 1 + l) - 2) {
-                    byte0 = 2;
-                }
-
-                for (int i2 = i - byte0; i2 <= i + byte0 && flag; i2++) {
-                    for (int l2 = k - byte0; l2 <= k + byte0 && flag; l2++) {
-                        if (i1 >= 0) {
-                            world.getClass();
-
-                            if (i1 < 128) {
-                                Block j3 = world.getBlock(i2, i1, l2);
-
-                                if (j3 != Blocks.air && j3 != BambooInit.sakuraleavs) {
-                                    flag = false;
-                                }
-
-                                continue;
-                            }
-                        }
-
-                        flag = false;
-                    }
-                }
+            if (i1 == posY) {
+                byte0 = 0;
             }
 
-            if (!flag) {
+            if (i1 >= (posY + 1 + l) - 2) {
+                byte0 = 2;
+            }
+
+            for (int i2 = posX - byte0; i2 <= posX + byte0 && flag; i2++) {
+                for (int l2 = posZ - byte0; l2 <= posZ + byte0 && flag; l2++) {
+                    if (i1 >= 0) {
+                        world.getClass();
+
+                        if (i1 < 128) {
+                            Block j3 = world.getBlock(i2, i1, l2);
+
+                            if (j3 != Blocks.air && j3 != BambooInit.sakuraleavs) {
+                                flag = false;
+                            }
+
+                            continue;
+                        }
+                    }
+
+                    flag = false;
+                }
+            }
+        }
+
+        if (!flag) {
+            return false;
+        }
+
+        Block j1 = world.getBlock(posX, posY - 1, posZ);
+
+        if (!(j1 == Blocks.grass || j1 == Blocks.dirt)) {
+            if (posY >= 128 - l - 1) {
                 return false;
             }
-
-            Block j1 = world.getBlock(i, j - 1, k);
-
-            if (j1 == Blocks.grass || j1 == Blocks.dirt) {
-                world.getClass();
-
-                if (j < 128 - l - 1) {
-                    break label1;
-                }
-            }
-
-            return false;
         }
-        world.setBlock(i, j - 1, k, Blocks.dirt, 0, 3);
 
-        for (int k1 = (j - 3) + l; k1 <= j + l; k1++) {
-            int j2 = k1 - (j + l);
+        world.setBlock(posX, posY - 1, posZ, Blocks.dirt, 0, 3);
+
+        for (int k1 = (posY - 3) + l; k1 <= posY + l; k1++) {
+            int j2 = k1 - (posY + l);
             int i3 = 1 - j2 / 2;
 
-            for (int k3 = i - i3; k3 <= i + i3; k3++) {
-                int l3 = k3 - i;
+            for (int k3 = posX - i3; k3 <= posX + i3; k3++) {
+                int l3 = k3 - posX;
 
-                for (int i4 = k - i3; i4 <= k + i3; i4++) {
-                    int j4 = i4 - k;
+                for (int i4 = posZ - i3; i4 <= posZ + i3; i4++) {
+                    int j4 = i4 - posZ;
 
                     if ((Math.abs(l3) != i3 || Math.abs(j4) != i3 || random.nextInt(2) != 0 && j2 != 0) && !world.getBlock(k3, k1, i4).isOpaqueCube()) {
                         world.setBlock(k3, k1, i4, BambooInit.sakuraleavs, type, 3);
@@ -115,10 +96,10 @@ public class WorldGenSakura extends WorldGenAbstractTree {
         }
 
         for (int l1 = 0; l1 < l; l1++) {
-            Block k2 = world.getBlock(i, j + l1, k);
+            Block k2 = world.getBlock(posX, posY + l1, posZ);
 
             if (k2 == Blocks.air || k2 == BambooInit.sakuraleavs) {
-                world.setBlock(i, j + l1, k, BambooInit.sakuralog, 0, 3);
+                world.setBlock(posX, posY + l1, posZ, BambooInit.sakuralog, 0, 3);
             }
         }
 
