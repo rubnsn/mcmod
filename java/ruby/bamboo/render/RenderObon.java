07dc2393c613788b1ddccff1bec3c218429b0aa7
rubnsn <whiteruby.0214@gmail.com>
2014-10-25 23:59:58 +0900

    1.7.2はおぼんに対する変更を削除

--- java/ruby/bamboo/render/RenderObon.java
+++ java/ruby/bamboo/render/RenderObon.java
@@ -1,7 +1,5 @@
 package ruby.bamboo.render;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import net.minecraft.client.Minecraft;
@@ -12,6 +10,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
@@ -34,15 +33,10 @@ public class RenderObon extends Render {
         itemRenderer.setRenderManager(RenderManager.instance);
     }
 
+    private EntityItem entityItem;
+
     @Override
     public void doRender(Entity entity, double d0, double d1, double d2, float f, float f1) {
-        this.doRender((EntityObon) entity, d0, d1, d2, f, f1);
-    }
-
-    private List<ItemStack> renderItems = new ArrayList<ItemStack>();
-    private float f;
-
-    public void doRender(EntityObon entity, double d0, double d1, double d2, float f, float f1) {
         GL11.glPushMatrix();
         this.bindEntityTexture(entity);
         GL11.glTranslatef((float) d0, (float) d1 + 0.2F, (float) d2);
@@ -51,54 +45,26 @@ public class RenderObon extends Render {
         model.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
         GL11.glPopMatrix();
 
-        GL11.glPushMatrix();
-        GL11.glScalef(0.7F, 0.7F, 0.7F);
-        renderItems.clear();
-        for (int i = 0; i < entity.MAX_SIZE; i++) {
-            ItemStack is = entity.getItemData(i);
-            if (is != null && is.getItem() != entity.EMPTY.getItem()) {
-                renderItems.add(is);
-            }
-        }
-        byte b = 0;
-        float distance = 0.25F;
-        float angle = 360 / (float) renderItems.size();
-        if (renderItems.size() != 1) {
-            for (ItemStack is : renderItems) {
-                double rad = Math.toRadians(angle * b++ + (entity.rotationYaw + 180.0F));
-                if (renderItems.size() == 3) {
-                    rad += 0.5;
-                } else if (renderItems.size() == 4) {
-                    rad -= 0.78;
-                    distance = 0.3F;
-                }
+        entityItem = ((EntityObon) entity).getEntityItem();
 
-                else if (renderItems.size() == 5) {
-                    rad -= 0.32;
-                    distance = 0.325F;
-                }
-                double offsetX = distance * Math.cos(rad);
-                double offsetZ = distance * Math.sin(rad);
-                doRenderItem(is, d0 + offsetX, d1 + 0.18, d2 + offsetZ, f, f1);
-            }
-        } else {
-            doRenderItem(renderItems.get(0), d0, d1 + 0.18, d2, 0, 0.0625F);
+        if (entityItem != null && entityItem.getEntityItem() != null && entityItem.getEntityItem().getItem() != null) {
+            doRenderItem(entityItem, d0, d1 + 0.18, d2 - 0.07, 0, 0);
         }
-        GL11.glPopMatrix();
     }
 
-    private ResourceLocation func_110796_a(ItemStack itemStack) {
-        return this.renderManager.renderEngine.getResourceLocation(itemStack.getItemSpriteNumber());
+    private ResourceLocation func_110796_a(EntityItem par1EntityItem) {
+        return this.renderManager.renderEngine.getResourceLocation(par1EntityItem.getEntityItem().getItemSpriteNumber());
     }
 
-    public void doRenderItem(ItemStack itemStack, double par2, double par4, double par6, float par8, float par9) {
-        this.bindTexture(func_110796_a(itemStack));
+    public void doRenderItem(EntityItem par1EntityItem, double par2, double par4, double par6, float par8, float par9) {
+        this.bindTexture(func_110796_a(par1EntityItem));
         this.random.setSeed(187L);
+        ItemStack itemstack = par1EntityItem.getEntityItem();
 
-        if (itemStack.getItem() != null) {
+        if (itemstack.getItem() != null) {
             GL11.glPushMatrix();
             GL11.glTranslatef((float) par2, (float) par4 + 0.1F, (float) par6);
-            byte b0 = 1;
+            byte b0 = getMiniBlockCount(itemstack);
             GL11.glEnable(GL12.GL_RESCALE_NORMAL);
             float f4;
             float f5;
@@ -106,31 +72,31 @@ public class RenderObon extends Render {
             int i;
             float f8;
 
-            if (itemStack.getItemSpriteNumber() == 1 && itemStack.getItem().requiresMultipleRenderPasses()) {
+            if (itemstack.getItemSpriteNumber() == 1 && itemstack.getItem().requiresMultipleRenderPasses()) {
                 GL11.glScalef(0.5128205F, 0.5128205F, 0.5128205F);
                 GL11.glTranslatef(0.0F, -0.05F, 0.0F);
 
-                for (int k = 0; k < itemStack.getItem().getRenderPasses(itemStack.getItemDamage()); ++k) {
+                for (int k = 0; k < itemstack.getItem().getRenderPasses(itemstack.getItemDamage()); ++k) {
                     this.random.setSeed(187L);
-                    IIcon icon = itemStack.getItem().getIcon(itemStack, k);
+                    IIcon icon = itemstack.getItem().getIcon(itemstack, k);
                     f8 = 1.0F;
-                    i = itemStack.getItem().getColorFromItemStack(itemStack, k);
+                    i = itemstack.getItem().getColorFromItemStack(itemstack, k);
                     f5 = (i >> 16 & 255) / 255.0F;
                     f4 = (i >> 8 & 255) / 255.0F;
                     f6 = (i & 255) / 255.0F;
                     GL11.glColor4f(f5 * f8, f4 * f8, f6 * f8, 1.0F);
-                    this.renderDroppedItem(itemStack, icon, b0, par9, f5 * f8, f4 * f8, f6 * f8, k);
+                    this.renderDroppedItem(par1EntityItem, icon, b0, par9, f5 * f8, f4 * f8, f6 * f8, k);
                 }
             } else {
                 GL11.glScalef(0.5128205F, 0.5128205F, 0.5128205F);
                 GL11.glTranslatef(0.0F, -0.05F, 0.0F);
-                IIcon icon1 = itemStack.getIconIndex();
-                int l = itemStack.getItem().getColorFromItemStack(itemStack, 0);
+                IIcon icon1 = itemstack.getIconIndex();
+                int l = itemstack.getItem().getColorFromItemStack(itemstack, 0);
                 f8 = (l >> 16 & 255) / 255.0F;
                 float f9 = (l >> 8 & 255) / 255.0F;
                 f5 = (l & 255) / 255.0F;
                 f4 = 1.0F;
-                this.renderDroppedItem(itemStack, icon1, b0, par9, f8 * f4, f9 * f4, f5 * f4);
+                this.renderDroppedItem(par1EntityItem, icon1, b0, par9, f8 * f4, f9 * f4, f5 * f4);
             }
 
             GL11.glDisable(GL12.GL_RESCALE_NORMAL);
@@ -160,16 +126,16 @@ public class RenderObon extends Render {
         return ret;
     }
 
-    private void renderDroppedItem(ItemStack itemStack, IIcon par2Icon, int par3, float par4, float par5, float par6, float par7) {
-        renderDroppedItem(itemStack, par2Icon, par3, par4, par5, par6, par7, 0);
+    private void renderDroppedItem(EntityItem par1EntityItem, IIcon par2Icon, int par3, float par4, float par5, float par6, float par7) {
+        renderDroppedItem(par1EntityItem, par2Icon, par3, par4, par5, par6, par7, 0);
     }
 
-    private void renderDroppedItem(ItemStack itemStack, IIcon par2Icon, int par3, float par4, float par5, float par6, float par7, int pass) {
+    private void renderDroppedItem(EntityItem par1EntityItem, IIcon par2Icon, int par3, float par4, float par5, float par6, float par7, int pass) {
         Tessellator tessellator = Tessellator.instance;
 
         if (par2Icon == null) {
             TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
-            ResourceLocation resourcelocation = texturemanager.getResourceLocation(itemStack.getItemSpriteNumber());
+            ResourceLocation resourcelocation = texturemanager.getResourceLocation(par1EntityItem.getEntityItem().getItemSpriteNumber());
             par2Icon = ((TextureMap) texturemanager.getTexture(resourcelocation)).registerIcon("missingno");
         }
 
